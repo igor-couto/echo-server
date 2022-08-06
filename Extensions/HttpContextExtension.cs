a52366b4ba5f59d101d49e574e32917bf5175fba
Igor Couto <igor.fcouto@gmail.com>
2022-08-06 12:16:40 -0300

    Initial Commit

---
+++ Extensions/HttpContextExtension.cs
@@ -0,0 +1,17 @@
+using System.Text;
+
+namespace Server;
+
+public static class HttpContextExtension
+{
+    public static async Task<string> GetRequestBody(this HttpContext httpContext)
+    {
+        if (!httpContext.Request.Body.CanSeek)
+            httpContext.Request.EnableBuffering();
+        httpContext.Request.Body.Position = 0;
+        var reader = new StreamReader(httpContext.Request.Body, Encoding.UTF8);
+        var requestBody = await reader.ReadToEndAsync().ConfigureAwait(false);
+        httpContext.Request.Body.Position = 0;
+        return requestBody ?? string.Empty;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var httpPort = 5000;
+var httpsPort = 5001;
+
+if (args is not null && args.Length > 0 && int.TryParse(args[0], out int port))
+{
+    httpPort = port;
+    httpsPort = port + 1;
+
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.ListenAnyIP(httpPort);
+        options.ListenAnyIP(httpsPort, configure => configure.UseHttps());
+    });
+}
+
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.SetMinimumLevel(LogLevel.Information);
+    logging.AddFilter("System", LogLevel.Error);
+    logging.AddFilter("Microsoft", LogLevel.Error);
+    logging.AddFilter("Microsoft.AspNetCore", LogLevel.Error);
+    logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Error);
+    logging.AddSimpleConsole(options =>
+        {
+            options.IncludeScopes = false;
+            options.SingleLine = false;
+            options.TimestampFormat = "hh:mm:ss ";
+        });
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.Use((context, next) =>
+{
+    context.Request.EnableBuffering();
+    return next();
+});
+
+app.UseMiddleware<LogMiddleware>();
+
+var emptyFunction = () => { };
+
+app.MapGet("/", emptyFunction);
+app.MapPost("/", emptyFunction);
+app.MapPut("/", emptyFunction);
+app.MapDelete("/", emptyFunction);
+
+Console.WriteLine($"Server listening on http://localhost:{httpPort} and https://localhost:{httpsPort} ...\n");
+
+app.Run();
